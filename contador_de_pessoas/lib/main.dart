e5589dd4699df1c4c2c08b4db0a3d6f41dcf0e66
Vitor Nicácio dos Santos <vitor.nicacio@imap.intranet>
2020-01-06 17:46:23 -0300

    first app

---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: "Contador de Pessoas",
+    home: Home())); 
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "Pode Entrar!";
+  void _changePeople(int delta){
+    setState(() {
+      _people+=delta;
+      if(_people < 0){
+        _infoText = "Mundo invertido?!";
+      }else if (_people <= 10){
+        _infoText = "Pode entrar";
+      }else{
+        _infoText = "Lotado!";
+      }
+    });
+
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000.00,),
+        Column (
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+          Text("Pessoas: $_people", 
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child:Text("+1", style: TextStyle(fontSize:40.0, color: Colors.white)),
+                  onPressed: () {
+                      _changePeople(1);
+                    },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child:Text("-1", style: TextStyle(fontSize:40.0, color: Colors.white)),
+                  onPressed: () {
+                      _changePeople(-1);
+                  },
+                ),
+              )
+            ]
+          ),
+          
+          Text("$_infoText!", 
+            style: TextStyle(color: Colors.white, 
+            fontStyle: FontStyle.italic,
+            fontSize: 30)
+          ),
+        ],
+      )
+      ],
+    );
+  }
+}
